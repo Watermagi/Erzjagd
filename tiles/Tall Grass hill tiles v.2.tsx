adad858f1dda6c2101d5dd9ecb82b923d7057632
Watermagi <123462158+Watermagi@users.noreply.github.com>
2023-05-10 08:25:46 +0200

    Add files via upload

---
+++ tiles/Tall Grass hill tiles v.2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Tall Grass hill tiles v.2" tilewidth="16" tileheight="16" tilecount="48" columns="6">
+ <image source="../../../Downloads/Sprout Lands - Sprites - Basic pack/Tilesets/ground tiles/new tiles/Tall Grass hill tiles v.2.png" width="96" height="128"/>
+</tileset>
---
+++ tiles/tileset_dirt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset_dirt" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../../../Downloads/Rocky Roads/Tilesets/tileset_dirt.png" width="192" height="96"/>
+</tileset>
