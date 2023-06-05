352064b81a11a2b5598fb635e86bf648df04c97d
Watermagi <123462158+Watermagi@users.noreply.github.com>
2023-06-05 22:44:37 +0200

    Update chars.py

--- chars.py
+++ chars.py
@@ -1,249 +1,149 @@
 import pygame
-import math
+import welt
 import chars
+import init
+import sys
 
 pygame.init()
 pygame.font.init()
-font = pygame.font.Font("font\mana.ttf", 20)
-highscore = 0
-stolen = 0
+BG = pygame.image.load("menu/Background.png")
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Erzjagd")
 
 
-def distance(x1, y1, x2, y2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("font/mana.ttf", size)
 
 
-class Player:
-    def __init__(self, game, x, y):
-        self.game = game
-        self.x = x
-        self.y = y
-        self.inventory = 0
-        self.max_inventory = 20
-        self.image = pygame.image.load("chars/spieler.png")
-        self.image = pygame.transform.scale(self.image,
-                                            (int(self.image.get_width() * 2), int(self.image.get_height() * 2)))
-        self.animations = {
-            "down": [(1, 1), (4, 1), (7, 1), (10, 1)],
-            "up": [(1, 4), (4, 4), (7, 4), (10, 4)],
-            "left": [(1, 7), (4, 7), (7, 7), (10, 7)],
-            "right": [(1, 10), (4, 10), (7, 10), (10, 10)],
-            "idle": [(1, 1), (4, 1)]
-        }
-        self.currentAnimation = "idle"
-        self.currentFrame = 0
-        self.animationDelay = 100
-        self.delayCounter = 0
-        self.ausdauer = 100
-        self.ausdauer_decrease_rate = 0.01
-
-    def draw_inventory_bar(self):
-        # Berechnet den aktuellen Erzstand des Spielers als Bruchteil des maximalen Inventarplatzes
-        currentCount = self.inventory
-        fracCount = currentCount / self.max_inventory
-        pygame.draw.rect(self.game.window, (255, 255, 255), pygame.Rect(80, 20, 200, 20))
-        pygame.draw.rect(self.game.window, (0, 255, 0), pygame.Rect(80, 20, int(200 * fracCount), 20))
-        text = font.render("Erz: {}/{}".format(currentCount, self.max_inventory), True, (0, 0, 0))
-        self.game.window.blit(text, (80, 50))
-
-    def draw_player(self):
-        player_image = self.image.subsurface(
-            pygame.Rect(self.animations[self.currentAnimation][self.currentFrame][0] * 32,
-                        self.animations[self.currentAnimation][self.currentFrame][1] * 32, 32, 32))
-        self.game.window.blit(player_image, (self.x, self.y))
-        name_surface = font.render("LKW", True, (0, 0, 255))
-        name_x = self.x + player_image.get_width() / 2 - name_surface.get_width() / 2
-        self.game.window.blit(name_surface, (name_x, self.y - 25))
-
-    def update_player(self):
-        movement = pygame.Vector2(0, 0)
-        self.draw_inventory_bar()
-
-        if pygame.key.get_pressed()[pygame.K_d]:
-            movement.x += 0.5
-            self.currentAnimation = "right"
-        if pygame.key.get_pressed()[pygame.K_a]:
-            movement.x -= 0.5
-            self.currentAnimation = "left"
-        if pygame.key.get_pressed()[pygame.K_w]:
-            movement.y -= 0.5
-            self.currentAnimation = "up"
-        if pygame.key.get_pressed()[pygame.K_s]:
-            movement.y += 0.5
-            self.currentAnimation = "down"
-
-        if pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_s] or \
-                pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_d]:
-            self.ausdauer -= self.ausdauer_decrease_rate
-
-        if movement.length() > 0:
-            movement.normalize_ip()
-            movement *= 0.5
-            new_x = self.x + movement.x
-            new_y = self.y + movement.y
-            if 90 <= new_x <= 1150:
-                self.x = new_x
-            if 90 <= new_y <= 640:
-                self.y = new_y
-
-            self.delayCounter += 1
-            if self.delayCounter >= self.animationDelay:
-                self.delayCounter = 0
-                self.currentFrame += 1
-                if self.currentFrame >= len(self.animations[self.currentAnimation]):
-                    self.currentFrame = 0
-        else:
-            self.currentFrame = 0
-            self.currentAnimation = "idle"
-
-    def collect_erz(self, mine):
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            player_rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
-            mine_rect = pygame.Rect(mine.x, mine.y, mine.image.get_width(), mine.image.get_height())
-            if player_rect.colliderect(mine_rect) and distance(self.x, self.y, mine.x, mine.y) < 30:
-                numOres = mine.erzCounter
-                if numOres > 0:
-                    space_available = self.max_inventory - self.inventory
-                    ores_to_collect = min(numOres, space_available)  # Collect only up to the available space
-                    ores_to_collect = int(ores_to_collect)
-                    self.inventory += ores_to_collect
-                    mine.erzCounter -= ores_to_collect
-
-    def deposit_erz(self, depot):
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            player_rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
-            depot_rect = pygame.Rect(depot.x, depot.y, depot.image.get_width(), depot.image.get_height())
-            if player_rect.colliderect(depot_rect) and distance(self.x, self.y, depot.x, depot.y) < 40:
-                chars.highscore += self.inventory * 100
-                self.inventory = 0
-
-    def refill_ausdauer(self, tankstelle):
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            player_rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
-            tankstelle_rect = pygame.Rect(tankstelle.x, tankstelle.y, tankstelle.image.get_width(),
-                                          tankstelle.image.get_height())
-            if player_rect.colliderect(tankstelle_rect) and distance(self.x, self.y, tankstelle.x,
-                                                                     tankstelle.y) < 50:
-                self.ausdauer = 100
-
-    def draw_ausdauer(self):
-        pygame.draw.rect(self.game.window, (255, 0, 0), (500, 20, 100, 20))
-        pygame.draw.rect(self.game.window, (0, 255, 0), (500, 20, self.ausdauer, 20))
-        text = font.render("Ausdauer: {}".format(self.ausdauer - (self.ausdauer % 1)), True, (0, 0, 0))
-        self.game.window.blit(text, (500, 50))
+def reset_game():
+    chars.highscore = 0
 
 
-class Helicopter:
-    def __init__(self, game, x, y):
-        self.game = game
-        self.x = x
-        self.y = y
-        self.inventory = 0
-        self.max_inventory = 5
-        self.wait = 0
-        self.image = pygame.image.load("chars/helicopter.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image,
-                                            (int(self.image.get_width() * 2), int(self.image.get_height() * 2)))
-        self.animations = {
-            "down": [(0, 0), (1, 0), (2, 0), (3, 0)],
-            "up": [(0, 1), (1, 1), (2, 1), (3, 1)],
-            "left": [(0, 2), (1, 2), (2, 2), (3, 2)],
-            "right": [(0, 3), (1, 3), (2, 3), (3, 3)],
-            "idle": [(0, 0), (1, 0), (2, 0), (3, 0)]
-        }
-        self.currentAnimation = "idle"
-        self.currentFrame = 0
-        self.animationDelay = 100
-        self.delayCounter = 0
+def play():
+    while True:
+        game = init.Game(1280, 720)
+        player = chars.Player(game, 620, 360)
+        mine = welt.Mine(game, 200, 200)
+        depot = welt.Depot(game, 1000, 500)
+        tankstelle = welt.Tankstelle(game, 1000, 200)
+        helicopter = chars.Helicopter(game, 200, 500)
+        base = welt.Basis(game, 200, 500)
 
-    def chase(self, player):
-        if self.inventory < self.max_inventory:
-            if self.wait < 4:
-                self.wait += 0.002
-                return
-            target_x = player.x
-            target_y = player.y
+        # Game loop
+        while game.running:
 
-            # Berechnen Sie die Richtung vom Helikopter zum Spieler
-            direction_x = target_x - self.x
-            direction_y = target_y - self.y
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-            direction_length = math.sqrt(direction_x ** 2 + direction_y ** 2)
-            if direction_length != 0:
-                direction_x /= direction_length
-                direction_y /= direction_length
+            # Draw game objects
+            game.draw_background()
+            game.draw_highscore()
+            mine.draw_mine()
+            depot.draw_depot()
+            tankstelle.draw_tankstelle()
+            base.draw_basis()
+            player.collect_erz(mine)
+            player.deposit_erz(depot)
+            player.refill_ausdauer(tankstelle)
+            player.draw_ausdauer()
+            player.draw_player()
+            helicopter.draw_helicopter()
+            mine.update_erz_counter()
+            player.update_player()
+            helicopter.chase(player)
+            helicopter.steal(player, helicopter)
+            helicopter.to_base(base)
+            helicopter.update_animation()
+            game.update_display()
 
-            # Bewegen Sie den Helikopter in Richtung des Spielers
-            speed = 0.2
-            self.x += direction_x * speed
-            self.y += direction_y * speed
+            # Check if Victory or Game Over
+            if player.ausdauer <= 0:
+                init.Game.draw_game_over(game)
+                reset_game()
+                break
+            elif chars.highscore >= 4000:
+                init.Game.draw_victory(game)
+                reset_game()
+                break
+            elif chars.highscore <= -100:
+                init.Game.draw_game_over(game)
+                reset_game()
+                break
 
-            # Aktualisieren Sie die Ausrichtung des Helikopters basierend auf der Richtung
-            if abs(direction_x) > abs(direction_y):
-                if direction_x > 0:
-                    self.currentAnimation = "right"
-                else:
-                    self.currentAnimation = "left"
-            else:
-                if direction_y > 0:
-                    self.currentAnimation = "down"
-                else:
-                    self.currentAnimation = "up"
+        # Continue to the main menu after the game is finished
+        pygame.time.delay(4000)  # Delay for 2 seconds before going back to the main menu
+        break  # Exit the play function and go back to the main menu
+    pygame.display.update()
 
-    def steal(self, player, helicopter):
-        player_rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
-        helicopter_rect = pygame.Rect(helicopter.x, helicopter.y, helicopter.image.get_width(),
-                                      helicopter.image.get_height())
-        if helicopter_rect.colliderect(player_rect) and distance(self.x, self.y, player.x, player.y) < 30:
-            if player.inventory >= 0:
-                space_available = self.max_inventory - self.inventory
-                ores_stolen = min(player.inventory, space_available)
-                self.inventory += ores_stolen
-                player.inventory -= ores_stolen
-                chars.stolen += 1
 
-    def to_base(self, base):
-        if self.inventory == self.max_inventory:
-            target_x = base.x
-            target_y = base.y
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-            direction_x = target_x - self.x
-            direction_y = target_y - self.y
+        SCREEN.fill("white")
 
-            direction_length = math.sqrt(direction_x ** 2 + direction_y ** 2)
-            if direction_length != 0:
-                direction_x /= direction_length
-                direction_y /= direction_length
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-            speed = 0.2
-            self.x += direction_x * speed
-            self.y += direction_y * speed
+        OPTIONS_BACK = init.Button(image=None, pos=(640, 460),
+                                   text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
 
-            if abs(direction_x) > abs(direction_y):
-                if direction_x > 0:
-                    self.currentAnimation = "right"
-                else:
-                    self.currentAnimation = "left"
-            else:
-                if direction_y > 0:
-                    self.currentAnimation = "down"
-                else:
-                    self.currentAnimation = "up"
-            if distance(self.x, self.y, base.x, base.y) < 5:
-                chars.highscore -= self.inventory * 100
-                self.inventory = 0
-                self.wait = 0
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
 
-    def draw_helicopter(self):
-        player_image = self.image.subsurface(
-            pygame.Rect(self.animations[self.currentAnimation][self.currentFrame][0] * 64,
-                        self.animations[self.currentAnimation][self.currentFrame][1] * 64, 64, 64))
-        self.game.window.blit(player_image, (self.x, self.y))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
 
-    def update_animation(self):
-        self.delayCounter += 1
-        if self.delayCounter >= self.animationDelay:
-            self.delayCounter = 0
-            self.currentFrame += 1
-            if self.currentFrame >= len(self.animations[self.currentAnimation]):
-                self.currentFrame = 0
+        pygame.display.update()
+
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = init.Button(image=pygame.image.load("menu/Play Rect.png"), pos=(640, 250),
+                                  text_input="PLAY", font=get_font(75), base_color="White", hovering_color="green")
+        OPTIONS_BUTTON = init.Button(image=pygame.image.load("menu/Options Rect.png"), pos=(640, 400),
+                                     text_input="OPTIONS", font=get_font(75), base_color="White",
+                                     hovering_color="green")
+        QUIT_BUTTON = init.Button(image=pygame.image.load("menu/Quit Rect.png"), pos=(640, 550),
+                                  text_input="QUIT", font=get_font(75), base_color="White", hovering_color="green")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+# Start the game
+main_menu()
